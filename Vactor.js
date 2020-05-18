887e611a6defb0cb906fe8968b2a12edb818bc82
Radosław Kuczyński <krooopel@gmail.com>
2020-05-18 23:07:59 +0200

    Added Vector.js

---
+++ Vactor.js
@@ -0,0 +1,20 @@
+class Vector {
+  constructor({ x, y }) {
+    this.x = x;
+    this.y = y;
+  }
+  reversX() {
+    this.x = -this.x;
+  }
+  reversY() {
+    this.y = -this.y;
+  }
+  get x() {
+    return this.x;
+  }
+  get y() {
+    return this.y;
+  }
+  set x(){}
+  set y(){}
+}
